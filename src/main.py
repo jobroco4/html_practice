6e57cbd8d4296eaf0419b54b304fd21600c95a8c
jobroco4 <jobroco4@gmail.com>
2026-02-21 17:08:03 -0700

    fixed public/docs confusion, added basepath argument to generate_pages_recursive calls

--- src/main.py
+++ src/main.py
@@ -3,12 +3,10 @@ import shutil
 import sys
 
 from copystatic import copy_files_recursive
-from gencontent import generate_page
 from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
-dir_path_docs = "./docs"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 default_basepath = "/"
@@ -20,13 +18,13 @@ def main():
         basepath = sys.argv[1]
 
     print("Deleting docs directory...")
-    if os.path.exists(dir_path_docs):
-        shutil.rmtree(dir_path_docs)
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
     print("Copying static files to docs directory...")
-    copy_files_recursive(dir_path_static, dir_path_docs)
+    copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 main()
